de41feec979d30733ac5a70184e7474580c559cb
tbajajUofA <tbajaj@ualberta.ca>
2025-10-25 16:49:13 -0600

    removed firebase dependeices

--- CodeBase/app/src/main/java/com/example/indeedgambling/Entrant.java
+++ CodeBase/app/src/main/java/com/example/indeedgambling/Entrant.java
@@ -6,6 +6,7 @@ public class Entrant extends Profile{
     private String email;
     private String phoneNum;
 
+    public Boolean joinEventWait(Event )
     public Entrant(String password, String profileName) {
         super(password, profileName);
     }
