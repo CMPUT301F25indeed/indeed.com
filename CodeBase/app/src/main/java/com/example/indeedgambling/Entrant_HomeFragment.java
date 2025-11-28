da6995d654a63ad50e621c43e0f3e3cdb00732f8
MirMahir <mirmahir@ualberta.ca>
2025-11-27 17:57:59 -0700

    XAN QR

--- CodeBase/app/src/main/java/com/example/indeedgambling/Entrant_HomeFragment.java
+++ CodeBase/app/src/main/java/com/example/indeedgambling/Entrant_HomeFragment.java
@@ -43,7 +43,8 @@ public class Entrant_HomeFragment extends Fragment {
         Button LogoutButton = view.findViewById(R.id.entrant_logout_button_home);
         TextView greeting = view.findViewById(R.id.entrant_greeting_home);
 
-        String[] optionsString = {"Browse", "History", "Profile", "Guidelines", "Notifications"};
+        String[] optionsString = {"Browse", "History", "Profile", "Guidelines", "Notifications", "Scan QR Code"};
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 requireContext(),
@@ -98,6 +99,9 @@ public class Entrant_HomeFragment extends Fragment {
             } else if (position == 4) {
                 NavHostFragment.findNavController(this)
                         .navigate(R.id.entrant_NotificationsFragment);
+            } else if (position == 5) {
+                NavHostFragment.findNavController(this)
+                        .navigate(R.id.action_entrantHomeFragment_to_scanQRFragment);
             }
         });
 
