e71889d5fbeda8498e9befb618068ca59169fa03
Xan17 <156242959+MANXAN13@users.noreply.github.com>
2025-12-01 16:48:15 -0700

    Add missing imports to Organizer_BrowseFragment
    Added imports for Button and NavHostFragment to support additional UI and navigation functionality in Organizer_BrowseFragment.

--- CodeBase/app/src/main/java/com/example/indeedgambling/Organizer_BrowseFragment.java
+++ CodeBase/app/src/main/java/com/example/indeedgambling/Organizer_BrowseFragment.java
@@ -16,10 +16,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
