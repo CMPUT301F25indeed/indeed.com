ea98a8c0ebec738a046a67d750fd8fcc7da0bcb2
tbajajUofA <tbajaj@ualberta.ca>
2025-11-30 21:21:34 -0700

    did an if statement or 2

--- CodeBase/app/src/main/java/com/example/indeedgambling/Admin_EventDetailsFragment.java
+++ CodeBase/app/src/main/java/com/example/indeedgambling/Admin_EventDetailsFragment.java
@@ -144,10 +144,11 @@ public class Admin_EventDetailsFragment extends Fragment {
 
         status.setText(event.getStatus());
 
-        if (event.getWaitingList() != null) {
-            total.setText(String.valueOf(event.getWaitingList().size()));
-        } else {
-            total.setText("0");
+
+        if (event.getWaitingList() != null&&event.RegistrationOpen()) {
+            total.setText(String.valueOf(event.getWaitingList().size()+"/"+event.getMaxWaitingEntrantsString()));
+        } else if (!(event.RegistrationOpen())&&event.getLostList() != null) {
+            total.setText(String.valueOf(event.getLostList().size()+"/"+event.getMaxWaitingEntrantsString()));
         }
 
     }
